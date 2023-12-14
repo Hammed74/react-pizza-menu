64afbd37e9727e2bd8130d52e5005a1f3cd2a16f
Rarri74 <Hammed74@gmail.com>
2023-12-14 00:31:36 -0500

    added pizzas

--- my-pizza-menu/src/App.jsx
+++ my-pizza-menu/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { pizzaData } from "../public/data";
+export default function App(){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+
+function Header(){
+  return (
+  <header className="header">
+    <h1 className="header">FAST REACT PIZZA CO</h1>
+  </header>
   )
 }
 
-export default App
+function Menu(){
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+        <ul className="menu-container">
+          {pizzaData.map((pizza) => {
+            return <Pizza pizzaObj={pizza} key={pizza.name}/> 
+          })}
+        </ul>
+    </main>
+  );
+}
+
+function Pizza({pizzaObj}){
+  return(
+   <li className="pizza-container">
+     <img className= {!pizzaObj.soldOut ? "pizza" : "pizza sold-out"} src={pizzaObj.photoName} alt="photo"/>
+     <div>
+        {!pizzaObj.soldOut ? <h3>{pizzaObj.price}</h3> : <h3 style={{fontWeight: 'bold', color: 'red'}} >SOLD OUT</h3>}
+       <h3>{pizzaObj.name}</h3>
+       <p className="ingredients">{pizzaObj.ingredients}</p>
+     </div>
+   </li>
+  )
+}
+
+function Footer(){
+const hour = new Date().getHours()
+const openHour = 12;
+const closeHour = 24;
+const isOpen = hour >= openHour && hour <= closeHour;
+console.log(hour)
+  return (
+    <footer style={{fontSize: '2rem'}}>
+      <StoreStatus status={isOpen}/>
+    </footer>
+  );
+}
+
+function StoreStatus({status}){
+  return(
+    <>
+<div style={{marginBottom: '2rem'}} className="order">
+        {status && <p>We're Open</p>}
+        {!status && <p>We're Closed</p> }
+      </div>
+      <button className="btn">Order</button>
+      </>
+  )
+}
